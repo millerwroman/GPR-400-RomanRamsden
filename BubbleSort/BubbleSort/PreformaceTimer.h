bf481783fc7c114014e1051609ebbab0a582cf0a
Miller Roman <Miller.roman@mymail.champlain.edu>
2021-03-12 14:48:48 -0500

    Updated timer

--- BubbleSort/BubbleSort/PreformaceTimer.h
+++ BubbleSort/BubbleSort/PreformaceTimer.h
@@ -3,6 +3,8 @@
 #include <chrono>
 #include <functional>
 #include <map>
+#include <atomic>
+#include <string>
 
 typedef std::chrono::high_resolution_clock Clock;
 typedef std::chrono::steady_clock::time_point Time;
@@ -40,6 +42,19 @@ struct RunInfo
 	float averageTime = 0;
 };
 
+struct RunInfoAtomic
+{
+	std::atomic_ulong minTime = 99999;
+	std::atomic_ulong maxTime = 0;
+	std::atomic_ulong averageTime = 0;
+};
+
+struct InstanceInfo
+{
+	Time StartTime;
+	Time EndTime;
+};
+
 
 class Timer
 {
@@ -57,7 +72,7 @@ public:
 			StartTimer("RunSetTime");
 			func();
 			float timeTaken = StopTimer("RunSetTime");
-			
+
 			info.averageTime += timeTaken;
 			info.minTime = (timeTaken < info.minTime ? timeTaken : info.minTime);
 			info.maxTime = (timeTaken > info.maxTime ? timeTaken : info.maxTime);
@@ -70,7 +85,7 @@ public:
 	void StartTimer(std::string timerName)
 	{
 		const Iterator item = instanceMap.find(timerName);
-		if(item != instanceMap.end())
+		if (item != instanceMap.end())
 		{
 			delete item->second;
 			item->second = nullptr;
@@ -98,6 +113,31 @@ public:
 	{
 		TimerInstance* instance = instanceMap.find(timerName)->second;
 		return instance->GetElapsedTime();
+	}
+
+	static float GetElapsedTime(InstanceInfo& info)
+	{
+		return (info.EndTime - info.StartTime).count();
+	}
+
+	static Time GetTime()
+	{
+		return Clock::now();
+	}
+
+	static RunInfo ProcessInstanceArray(InstanceInfo* arr, int size)
+	{
+		RunInfo info;
+		unsigned long long averageTime;
+		for(int i=0;i<size; ++i)
+		{
+			float timeTaken = GetElapsedTime(arr[i]);
+			averageTime += timeTaken;
+			info.minTime = (timeTaken < info.minTime ? timeTaken : info.minTime);
+			info.maxTime = (timeTaken > info.maxTime ? timeTaken : info.maxTime);
+		}
+
+		return info;
 	}
 
 	~Timer()
