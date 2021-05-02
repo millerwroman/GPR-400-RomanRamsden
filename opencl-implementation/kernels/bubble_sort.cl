a56c7c3a3faf836f6e651fa3b051bb257ee08181
Connor Ramsden <connor.ramsden@mymail.champlain.edu>
2021-05-02 18:53:10 -0400

    bubble sort works well enough for testing

--- opencl-implementation/kernels/bubble_sort.cl
+++ opencl-implementation/kernels/bubble_sort.cl
@@ -1,5 +1,48 @@
 // Basic OpenCL Bubblesort
-__kernel void BubbleSort(__global int * arr, const int n)
-{
 
+// Swap helper method
+// void swap(int* x, int* y)
+// {
+// 	int temp = *x;
+// 	*x = *y;
+// 	*y = temp;
+// }
+// 
+// // Sorting helper method
+// void sort(int* x, int* y, char dir)
+// {
+// 	if ((*x > *b) == dir)
+// 		swap(x, y);
+// }
+
+__kernel void bubble_sort(
+	__global int* in,
+	__global int* out,
+	const unsigned int count)
+{
+	int id = get_global_id(0);
+
+	// Handles out of bounds stuff
+	if (id < count) {
+
+		// Get two numbers, index and right of index
+		int left = in[id];
+		int right = in[id + 1];
+
+		if(left > right)
+		{
+			int temp = left;
+			left = right;
+			right = temp;
+		}
+
+		out[id] = left;
+		out[id+1] = right;
+
+		// return;
+		// 
+		// Sort(&left, &right, 1);
+		// out[id] = left;
+		// out[id + 1] = right;
+	}
 }
--- opencl-implementation/src/main.cpp
+++ opencl-implementation/src/main.cpp
@@ -12,6 +12,7 @@
 #include "err_code.h"
 
 const unsigned int RANDOM_SEED = 2435345;
+constexpr int len(10000);
 
 int main(void)
 {
@@ -19,24 +20,59 @@ int main(void)
 	SetSeed(RANDOM_SEED);
 	Timer* timer = new Timer();
 
+	// Create an array to hold integers
+	std::vector<int> originVector(len);
+
 	// Populate an array with random integers
-	std::vector<int> arrayIn(LENGTH);
-	
-	for(int i = 0; i < LENGTH; ++i)
+	for (int i = 0; i < len; ++i)
 	{
-		arrayIn[i] = static_cast<int>(GetRandomFloat32_Range(0, (5 * LENGTH)));
-		std::cout << arrayIn[i] << std::endl;
+		originVector[i] = static_cast<int>(GetRandomFloat32_Range(0, (5 * len)));
 	}
-	
+
 	try
 	{
 		// RunVectorAdd(timer);
 		// RunPrimeFactor(timer);
-		
+
+		cl::Buffer inputVector;
+		cl::Buffer outputVector;
+
 		cl::Context context(DEVICE);
-		cl::Program program(context, LoadKernel("bubble_sort.cl"));
-		cl::CommandQueue commandQueue(context);
 				
+		cl::Program program(context, LoadKernel("bubble_sort.cl"), true);
+
+		cl::CommandQueue commandQueue(context);
+
+		cl::make_kernel<cl::Buffer, cl::Buffer, int> bubbleSort(program, "bubble_sort");
+
+		inputVector = cl::Buffer(context, originVector.begin(),
+			originVector.end(), true);
+		outputVector = cl::Buffer(context, CL_MEM_WRITE_ONLY,
+			sizeof(int) * len);
+
+		timer->StartTimer("Main");
+
+		bubbleSort(
+			cl::EnqueueArgs(
+				commandQueue, cl::NDRange(len)),
+			inputVector, outputVector, len);
+
+		commandQueue.finish();
+
+		timer->StopTimer("Main");
+
+		std::cout << "\nThe kernels ran in " << std::to_string(timer->GetElapsedTime("Main"))
+			<< " seconds\n" << std::endl;
+
+		std::vector<int> destinationVector(len);
+
+		cl::copy(commandQueue, outputVector, destinationVector.begin(), destinationVector.end());
+
+		// for(int i = 0; i < destinationVector.size(); ++i)
+		// {
+		// 	std::cout << originVector[i] << "\t";
+		// 	std::cout << destinationVector[i] << std::endl;
+		// }
 	}
 	catch (cl::Error err)
 	{
